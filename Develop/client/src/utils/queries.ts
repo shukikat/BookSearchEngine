e641a8ff0fcbcc7f8ef6207d766cdb5cf772edd8
shukikat <kathuriashuki@gmail.com>
2024-11-28 22:25:43 -0500

    working on the frontend logic, added query.ts file

---
+++ Develop/client/src/utils/queries.ts
@@ -0,0 +1,25 @@
+import {gql} from '@apollo/client'
+
+export const GET_ME=gql`
+query me {
+
+me{
+
+_id
+username
+email
+savedBooks {
+bookId
+title
+authors
+description
+image
+link
+
+}
+bookCount
+
+}
+
+}
+`;
