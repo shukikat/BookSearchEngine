ed24c39827ffc8b2a09acef4eb59770ab799fc6b
shukikat <kathuriashuki@gmail.com>
2024-11-30 22:11:51 -0500

    made some final corrections to save books

--- Develop/client/src/pages/SavedBooks.tsx
+++ Develop/client/src/pages/SavedBooks.tsx
@@ -97,9 +97,9 @@ const SavedBooks = () => {
   };
 
   // if data isn't here yet, say so
-  if (!userDataLength) {
-    return <h2>LOADING...</h2>;
-  }
+  // if (!userDataLength) {
+  //   return <h2>LOADING...</h2>;
+  // }
 
   return (
     <>
