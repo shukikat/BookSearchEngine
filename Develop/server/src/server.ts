c1c896ceeee5df9ae13cd0a43a9d50292ff5c598
shukikat <kathuriashuki@gmail.com>
2024-12-02 21:47:08 -0500

    updated server.ts

--- Develop/server/src/server.ts
+++ Develop/server/src/server.ts
@@ -8,7 +8,10 @@ import { ApolloServer } from '@apollo/server';
 import { expressMiddleware } from '@apollo/server/express4';
 import { typeDefs, resolvers } from './schemas/index.js';
 import { authenticateToken } from './utils/auth.js';
-
+import { fileURLToPath } from 'url';
+import {dirname} from 'path';
+const __filename=fileURLToPath(import.meta.url)
+const __dirname=dirname(__filename);
 const server= new ApolloServer({
   typeDefs, 
   resolvers,
